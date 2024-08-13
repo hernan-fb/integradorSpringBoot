cfe5a9786d056a88b7d34e2e97ebdc8570368d24
Hernan Brando - ZENI <hbrando@zeni.com.ar>
2024-08-13 00:45:56 -0300

    borrado proyectos que no funcionan

--- EmailSender - copia/src/main/java/com/api/email/emailsender/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.api.email.emailsender.service;
-
-import com.api.email.emailsender.entity.EmailDetails;
-import org.springframework.http.ResponseEntity;
-
-public interface EmailService {
-    ResponseEntity<String> sendSimpleMail(EmailDetails details);
-    ResponseEntity<String> sendMailWithAttachment(EmailDetails details);
-}
